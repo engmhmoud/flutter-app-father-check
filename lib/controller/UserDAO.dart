a4685b8bfc9290fffe45a1104c05c2da2ea0d87a
mhmoudsabry <mhmoudsabry25@gmail.com>
2021-07-03 06:47:57 +0200

    Added mysql setting , login , register ,select data

--- lib/controller/UserDAO.dart
+++ lib/controller/UserDAO.dart
@@ -1,11 +1,11 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 // import 'package:hive/hive.dart';
 import 'package:parent_check_app/Model/User.dart' as model;
-
-import 'package:firebase_auth/firebase_auth.dart';
-import 'dart:async';
-import 'package:flutter/cupertino.dart';
 
 class UserDAO with ChangeNotifier {
   static final FirebaseAuth _auth = FirebaseAuth.instance;
--- lib/controller/notificationDAO.dart
+++ lib/controller/notificationDAO.dart
@@ -1,36 +1,36 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
 
-class NotificationDAO {
-  FirebaseMessaging messaging = FirebaseMessaging.instance;
-  request() async {
-    NotificationSettings settings = await messaging.requestPermission(
-      alert: true,
-      announcement: false,
-      badge: true,
-      carPlay: false,
-      criticalAlert: false,
-      provisional: false,
-      sound: true,
-    );
+// class NotificationDAO {
+//   FirebaseMessaging messaging = FirebaseMessaging.instance;
+//   request() async {
+//     NotificationSettings settings = await messaging.requestPermission(
+//       alert: true,
+//       announcement: false,
+//       badge: true,
+//       carPlay: false,
+//       criticalAlert: false,
+//       provisional: false,
+//       sound: true,
+//     );
 
-    print('User granted permission: ${settings.authorizationStatus}');
-  }
+//     print('User granted permission: ${settings.authorizationStatus}');
+//   }
 
-// foregound
-  listen() {
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print('Got a message whilst in the foreground!');
-      print('Message data: ${message.data}');
+// // foregound
+//   listen() {
+//     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+//       print('Got a message whilst in the foreground!');
+//       print('Message data: ${message.data}');
 
-      if (message.notification != null) {
-        print('Message also contained a notification: ${message.notification}');
-      }
-    });
-    var token = FirebaseMessaging.instance.getToken();
-  }
+//       if (message.notification != null) {
+//         print('Message also contained a notification: ${message.notification}');
+//       }
+//     });
+//     var token = FirebaseMessaging.instance.getToken();
+//   }
 
-  Future<void> subscripeToTopic(String topic) async {
-    await FirebaseMessaging.instance.subscribeToTopic(topic);
-  }
-}
+//   Future<void> subscripeToTopic(String topic) async {
+//     await FirebaseMessaging.instance.subscribeToTopic(topic);
+//   }
+// }
